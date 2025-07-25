51c181a605f93dca6f4e7c3c067614a726edec85
Risman Hadi Nata <rismanh158@gmail.com>
2025-07-25 14:51:34 +0700

    Menambahkan Route Untuk SuperUser

---
+++ app/Http/Controllers/superuser/KategoriTugasController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Superuseruru;
+
+use App\Http\Controllers\Controller;
+use App\Models\KategoriTugas;
+use Illuminate\Http\Request;
+use App\Models\AktivitasSiswa;
+
+class KategoriTugasController extends Controller
+{
+    public function index()
+    {
+        $kategoris = KategoriTugas::oldest()->paginate(10);
+        return view('superuser.data-kategori.index', compact('kategoris'));
+    }
+
+    public function create()
+    {
+        return view('superuser.data-kategori.create');
+    }
+
+    public function store(Request $request)
+{
+    $request->validate([
+        'nama_kategori' => 'required|string|max:255',
+        'deskripsi' => 'nullable|string',
+        'keywords' => 'nullable|string',
+    ]);
+
+    KategoriTugas::create([
+        'nama_kategori' => $request->nama_kategori,
+        'deskripsi' => $request->deskripsi,
+        'keywords' => $request->keywords,
+    ]);
+
+    return redirect()->route('superuser.data-kategori.index')->with('success', 'Kategori berhasil ditambahkan.');
+}
+
+
+    public function edit($id)
+    {
+        $kategori = KategoriTugas::findOrFail($id);
+        return view('superuser.data-kategori.edit', compact('kategori'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama_kategori' => 'required|string|max:255',
+            'deskripsi' => 'nullable|string',
+        ]);
+
+        $kategori = KategoriTugas::findOrFail($id);
+        $kategori->update($request->all());
+
+        return redirect()->route('superuser.data-kategori.index')->with('success', 'Kategori berhasil diperbarui');
+    }
+
+    public function destroy($id)
+    {
+        $kategori = KategoriTugas::findOrFail($id);
+        $kategori->delete();
+
+        return redirect()->route('superuser.data-kategori.index')->with('success', 'Kategori berhasil dihapus');
+    }
+
+    function deteksiKategori($kalimat)
+{
+    $kalimat = strtolower($kalimat);
+    $kategoris = KategoriTugas::all();
+
+    foreach ($kategoris as $kategori) {
+        $keywords = explode(',', strtolower($kategori->keywords ?? ''));
+
+        foreach ($keywords as $keyword) {
+            $keyword = trim($keyword);
+            if (str_contains($kalimat, $keyword)) {
+                return $kategori->id;
+            }
+        }
+    }
+
+    return null; // jika tidak cocok
+}
+}
